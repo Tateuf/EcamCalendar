e8226c5789c2dee65255beab85a889dee4361fa0
Mustafalou <yilmaz121030@hotmail.be>
2021-11-19 11:52:31 +0100

    Setup

--- src/main/java/com/ecam/Calendar/CalendarApplication.java
+++ src/main/java/com/ecam/Calendar/CalendarApplication.java
@@ -2,10 +2,9 @@ package com.ecam.Calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class CalendarApplication {
-
+	DBConnect();
 	public static void main(String[] args) {
 		SpringApplication.run(CalendarApplication.class, args);
 	}
