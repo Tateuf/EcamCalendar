9cd5025952947053d88490c7a9e1e6a6e698426d
Tateuf <logan.noel.h@gmail.com>
2021-11-19 08:50:57 +0100

    Initial commit

---
+++ src/main/java/com/ecam/Calendar/web/controller/LectureController.java
@@ -0,0 +1,12 @@
+package com.ecam.Calendar.web.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LectureController {
+    @RequestMapping(value="/Produits", method= RequestMethod.GET)
+    public String listeProduits() {
+        return "Un exemple de produit";
+    }
+}
