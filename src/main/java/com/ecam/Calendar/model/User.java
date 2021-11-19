355105227d3d98833cb54038d13b7bb6ef297c5d
Tateuf <logan.noel.h@gmail.com>
2021-11-19 10:30:12 +0100

    Add User model and complete UserController

--- src/main/java/com/ecam/Calendar/model/User.java
+++ src/main/java/com/ecam/Calendar/model/User.java
@@ -1,4 +1,59 @@
 package com.ecam.Calendar.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String name;
+    private String firstname;
+    private final String code;
+    private final String role;
+
+    public User(String name, String firstname, String code, String role) {
+        this.name = name;
+        this.firstname = firstname;
+        this.code = code;
+        this.role = role;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", code='" + code + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+
+    public static List<User> read(String code) {
+        //TODO link to database
+        //Database.readLecture()
+        return new ArrayList<User>();
+    }
 }
--- src/main/java/com/ecam/Calendar/web/controller/UserController.java
+++ src/main/java/com/ecam/Calendar/web/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.ecam.Calendar.web.controller;
+import com.ecam.Calendar.model.Lecture;
+import com.ecam.Calendar.model.User;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
+@RestController
 public class UserController {
+
+    @GetMapping("/User/Read")
+    public List<User> UserRead(@RequestParam(value = "code") String code) {
+        return User.read(code);
+    }
+
 }
