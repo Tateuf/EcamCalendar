e0e1544152dfcfd3b028beb6b53c59c8c6e8c405
Tateuf <logan.noel.h@gmail.com>
2021-11-21 10:09:28 +0100

    Add HttpRequestExample

--- src/main/java/com/ecam/Calendar/CalendarApplication.java
+++ src/main/java/com/ecam/Calendar/CalendarApplication.java
@@ -2,11 +2,17 @@ package com.ecam.Calendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 @SpringBootApplication
 public class CalendarApplication {
-	public static void main(String[] args) throws ClassNotFoundException {
-		DBConnect db = new DBConnect();
-		db.Ping();
+	public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException, URISyntaxException {
+		//DBConnect db = new DBConnect();
+		//db.Ping();
+		HttpRequestExample httpRequestExample = new HttpRequestExample();
+		httpRequestExample.Print();
 		SpringApplication.run(CalendarApplication.class, args);
 	}
 
