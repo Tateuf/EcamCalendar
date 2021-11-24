db5908a914f51da8f7393d6d868339b7c703dd11
Chaimae <mottakichaimae@gmail.com>
2021-11-24 16:52:10 +0100

    added a Unit Test for Ping() in DBConnect

--- src/main/java/com/ecam/Calendar/DBConnect.java
+++ src/main/java/com/ecam/Calendar/DBConnect.java
@@ -6,36 +6,36 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
 public class DBConnect {
-    public void Ping(){
+    public int Ping() {
         String deviceUser = "pi";
         String devicePassWord = "1453";
         String deviceHostName = "109.132.191.163";
-        int sshPort =22;
+        int sshPort = 22;
         int dbPort = 3306;
         String dbUser = "user";
         String dbHostName = "127.0.0.1";
         String dbPassWord = "user";
-        String database="Logiciel";
+        String database = "Logiciel";
         Session session;
-        try{
+        try {
             java.util.Properties config = new java.util.Properties();
             config.put("StrictHostKeyChecking", "no");
             JSch jsch = new JSch();
-            session = jsch.getSession(deviceUser,deviceHostName,sshPort);
+            session = jsch.getSession(deviceUser, deviceHostName, sshPort);
             session.setPassword(devicePassWord);
             session.setConfig(config);
-            int forwardedPort = session.setPortForwardingL(0,dbHostName,dbPort);
+            int forwardedPort = session.setPortForwardingL(0, dbHostName, dbPort);
             System.out.println("Establishing Connection ...");
             session.connect();
             System.out.println("Connection Established");
             DriverManager.registerDriver(new com.mysql.jdbc.Driver());
             System.out.println("Driver ok");
-            Connection con = DriverManager.getConnection("jdbc:mysql://"+dbHostName+":"+forwardedPort+"/"+database,dbUser,dbPassWord);
+            Connection con = DriverManager.getConnection("jdbc:mysql://" + dbHostName + ":" + forwardedPort + "/" + database, dbUser, dbPassWord);
             System.out.println("Db Connection established");
-        }catch (JSchException | SQLException e){
+        } catch (JSchException | SQLException e) {
             e.printStackTrace();
+            return 1;
         }
-
+        return 0;
     }
-
 }
