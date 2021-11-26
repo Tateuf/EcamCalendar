a88b4064705c0a86345cb88a1966cca9542c34b2
Tateuf <logan.noel.h@gmail.com>
2021-11-26 11:21:48 +0100

    Add function in room

--- src/main/java/com/ecam/Calendar/db/dbRoom.java
+++ src/main/java/com/ecam/Calendar/db/dbRoom.java
@@ -1,4 +1,14 @@
 package com.ecam.Calendar.db;
 
 public class dbRoom {
+    public static boolean checkCapacity(String room,String code) {
+        //compare number of student and the capacity of a room
+        return true;
+    }
+
+    public static boolean checkAvaibility(String room, String day, String start, String end) {
+        //check is a room is avaible for this day and this hour.
+        return true;
+    }
 }
+
--- src/main/java/com/ecam/Calendar/web/controller/RoomController.java
+++ src/main/java/com/ecam/Calendar/web/controller/RoomController.java
@@ -1,5 +1,5 @@
 package com.ecam.Calendar.web.controller;
-import com.ecam.Calendar.model.Lecture;
+
 import com.ecam.Calendar.model.Room;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
@@ -9,7 +9,19 @@ import java.util.List;
 public class RoomController {
 
     @GetMapping("/Room/Read")
-    public List<String> roomRead(@RequestParam(value = "room") ArrayList<String> room){
+    public List<String> roomRead(@RequestParam(value = "room") ArrayList<String> room) {
         return Room.grabRoomFromDB(room);
+    }
+
+    public String checkCapacity(String room,String code){
+        return String.valueOf(Room.checkCapacity(room, code));
+    }
+
+    @GetMapping("Room/Available")
+    public String checkAvailability(@RequestParam(value = "room") String room,
+                                     @RequestParam(value = "day") String day,
+                                     @RequestParam(value = "start") String start,
+                                     @RequestParam(value = "end") String end){
+    return String.valueOf(Room.checkAvaibility(room,day,start,end));
     }
 }
